d539903a7919788f612e668b2db4a503f51f1e56
SoftwareDoctor <andrea_italiano87@yahoo.com>
2024-07-22 16:05:35 +0200

    commit angular new3

--- angularFE/src/app/app.component.ts
+++ angularFE/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angularFE';
+  title = 'Software Doctor';
+  subtitle: string = 'Software Doctor';
 }
